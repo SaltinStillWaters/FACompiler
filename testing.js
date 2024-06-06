63c3c32e8284572e21e0469a685da49417836606
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-06-06 16:06:22 +0800

    Added references for study.

---
+++ testing.js
@@ -0,0 +1,34 @@
+
+const baseURL='https://feu.instructure.com'
+
+const quizURL = `${baseURL}api/v1/courses/${courseID}/quizzes/${quizID}/`;
+const submissionsURL = quizURL + 'submissions';
+
+await(Promise.all([fetch(quizURL), fetch(submissionsURL)])
+    .then(([resQuiz, resSubmissions]) => {
+        return (Promise.all([resQuiz.text(), resSubmissions.text()])
+            .then(([resQuiz, resSubmissions]) => {
+                return [JSON.parse(resQuiz), JSON.parse(resSubmissions).quiz_submissions];
+            }))
+    })
+    .then(([resQuiz, resSubmissions]) => {
+        const assignmentID = resQuiz.assignment_id;
+        const userID = resSubmissions[resSubmissions.length - 1].user_id;
+
+        if (!assignmentID) {
+            throw new Error('Unable to retrieve assignmentID');
+        } else if (!userID) {
+            throw new Error('Unable to retrieve userID');
+        }
+        console.log(resQuiz);
+    }));
+//     const submissionsHistoryURL = `${baseURL}api/v1/courses/${courseID}/assignments/${assignmentID}/submissions/${userID}?include[]=submission_history`;
+//     return fetch(submissionsHistoryURL);
+// })
+// .then((res) => {
+//     return (res.text()
+//         .then((res) => JSON.parse(res)))
+// })
+// .then((res) => {
+//     return res.submission_history;
+// }));
