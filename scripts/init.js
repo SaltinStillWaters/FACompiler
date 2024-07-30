a3ad7a4f5511b49d66ef0514ec4a65e737a764a4
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-07-30 15:23:14 +0800

    implemented get and store data

--- scripts/init.js
+++ scripts/init.js
@@ -1,14 +1,40 @@
-getCourseID();
+storeDataLocal('courseID', getCourseID());
+getDataLocal('courseID', function(val) {
+    if (val) {
+        console.log('Course ID is ' + val);
+    } else {
+        console.log('No Course ID found');
+    }
+});
+
+getFANumber();
+let currURL = window.location.href;
 
 function getCourseID() {
-    let URL = window.location.href;
-    let match = URL.match(/courses\/(\d+)/);
+    let currURL = window.location.href;
+    let match = currURL.match(/courses\/(\d+)/);
     
-    //add error handling
-    let courseID = match[1];
-    console.log('course id: ' + courseID);
-    
-    //add error handling
-    let FANumber = document.querySelector('header.quiz-header').querySelector('h1').textContent;
-    console.log('FA number: ' + FANumber);
+    return match[1];
+}
+
+function getFANumber() {
+    return document.querySelector('header.quiz-header').querySelector('h1').textContent;
+}
+
+function storeDataLocal(key, val) {
+    chrome.storage.local.set({[key] : val}, function() {
+        console.log('key: ' + key + ', val: ' + val + ' is set');
+    });
+}
+
+function getDataLocal(key, callback) {
+    chrome.storage.local.get([key], function(result) {
+        if (result[key]) {
+            console.log('val of key: ' + key + ' is: ' + result[key]);
+            callback(result[key]);
+        } else {
+            console.log('key: ' + key + ' not found');
+            callback(null);
+        }
+    });
 }
